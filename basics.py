6c91147073d73eda36b58a90cfa89c5baf5b56a4
shrutiTechenthu <shrutishukla12ab@gmail.com>
2026-07-20 14:57:56 +0530

    add: python calculator
    Implemented a basic Python calculator that performs addition, subtraction, multiplication, division, and exponentiation based on user input.

---
+++ basics.py
@@ -0,0 +1,25 @@
+// Python calculator 
+a=int(input("1st number:"))
+b=int(input("2nd number:"))
+print(a,b)
+print("PYTHON CALCULATOR ")
+print("Operations available: +,-,*,/,**")
+option=input("enter the arithmetic operation ")
+if option== "+":
+    Add=a+b
+    print(Add)
+elif option== "-":
+    Sub=a-b
+    print(Sub)
+elif option=="*":
+    Multi=a*b and b*a
+    print(Multi)
+elif option== "/":
+    Divi=a/b and b/a
+    print(Divi)
+elif option== "**":
+    Expo=a**b and b**a
+    print(Expo)
+else:
+    print("Operation not available ")
+  
